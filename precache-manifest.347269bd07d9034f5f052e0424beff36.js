dd65d08a431906724b5755b88b3aecad3de0bada
Joshua McGehee <mcgehee.joshua@gmail.com>
2019-08-10 18:42:21 -0600

    Updates

---
+++ precache-manifest.347269bd07d9034f5f052e0424beff36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800f620bba5a78c36aacffe513d0f6f5",
+    "url": "/PlowShare_frontend/index.html"
+  },
+  {
+    "revision": "177e6914329016aa642d",
+    "url": "/PlowShare_frontend/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "032ea55f1e087ddfcc52",
+    "url": "/PlowShare_frontend/static/js/2.3080cdec.chunk.js"
+  },
+  {
+    "revision": "177e6914329016aa642d",
+    "url": "/PlowShare_frontend/static/js/main.008c3082.chunk.js"
+  },
+  {
+    "revision": "6ba6032843ae8724c29a",
+    "url": "/PlowShare_frontend/static/js/runtime~main.7f4b9ed4.js"
+  }
+]);
